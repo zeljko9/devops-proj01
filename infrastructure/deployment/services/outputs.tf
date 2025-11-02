18f9180547ffcaa6e36f73fdf2a9d362b7d91c58
Zeljko Miloradovic <zeljko.miloradovic@coop.co.uk>
2025-11-02 18:37:25 +0100

    Changes regarding known_hosts pt2

--- infrastructure/deployment/services/outputs.tf
+++ infrastructure/deployment/services/outputs.tf
@@ -5,6 +5,6 @@ output "public_ip_address" {
 output "ansible_inventory" {
   value = templatefile("${path.module}/templates/inventory.tpl", {
     env_name = var.environment.environment_id
-    ip       = module.vm_service.public_ip_addresses
+    ip       = module.vm_service.public_ip_address
   })
 }
